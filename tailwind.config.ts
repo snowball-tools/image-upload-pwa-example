8bbab0ad4a9c36ebbc2bdcf0825924ba7f0c2e9d
Vivian Phung <dev+github@vivianphung.com>
2024-03-21 22:29:10 -0400

    tailwind.config.js -> tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import { Config } from "tailwindcss";
+
+export default {
+	content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extent: {},
+	},
+	plugins: [],
+} satisfies Config;
